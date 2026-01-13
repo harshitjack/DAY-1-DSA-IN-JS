99386845076b9329b1ae252f8c5c5c3ffb116b87
harshit <mishraharshit255@gamil.com>
2026-01-13 19:24:11 +0530

    day 1dsa-js

---
+++ index.js
@@ -0,0 +1,41 @@
+                                                           // DAY-1
+
+
+                        //  Q-1  how to get value from user 
+
+                         let age = prompt('enter the password ')
+                         
+                         console.log(age)
+
+
+                        // Q-2 swap two variable via 3 method  
+
+                        // let a = 5 
+                        //  let b = 10 
+                        //   let c = a 
+                        //   a=b 
+                        //   b =c 
+                        //   console.log(a,b)
+
+                        //   second method 
+                        // let a =10
+                        // let b = 50 
+                        //  a = a+b  //60 =a 
+                        //  b = a-b // 10 = a 
+                        //  a = a-b // 50 = a 
+                            
+                         console.log(a, b)
+
+                         // third method 
+
+                         let a = 10 
+                         let b = 20 
+
+                         [a,b] = [b,a]
+console.log(a,b)
+                        
+
+
+
+
+
